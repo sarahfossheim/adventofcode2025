de6a981d830307910ef2db79c3b73e18b8787eeb
Sarah Fossheim <sarah@fossheim.io>
2025-12-03 14:02:17 +0100

    solve day 3 part 1

---
+++ 03/part1.py
@@ -0,0 +1,26 @@
+import csv
+
+with open('input.csv', mode='r', newline='', encoding='utf-8') as file:
+  csv_reader = csv.reader(file)
+  battery_sum = 0
+  for row in csv_reader:
+    # Get an array of characters in the bank
+    bank = row[0]
+    bank_list = list(bank) # if ints: [int(battery) for battery in list(bank)]
+
+    # Get largest number
+    # Disregard the last item of the bank, since we need double digits
+    last_id = len(bank_list) - 1
+    largest = max(bank_list[:last_id])
+    largest_id = bank_list.index(largest)
+
+    # Get the largest number that comes after
+    largest_2 = max(bank_list[largest_id+1:])
+
+    # Convert both to a number
+    battery = largest + largest_2
+    battery = int(battery)
+
+    battery_sum += battery
+
+  print('⭐️ Battery sum:', battery_sum)
